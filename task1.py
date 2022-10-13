931bf1b28d9e182c2fc02ba68366f4b2701058dd
tv-12-melnyk-ihor <114071900+tv-12-melnyk-ihor@users.noreply.github.com>
2022-10-13 18:55:42 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,31 +1,31 @@
 class Rectangle:
-    def __init__(self):
-        self.length = 1.0
-        self.width = 1.0
+    def __init__(self, leng = 1.0, wid = 1.0):
+        self.length = leng
+        self.width = wid
 
     @property
-    def mylength(self):
-        return self.length
+    def length(self):
+        return self.__length
 
-    @mylength.setter
-    def mylength(self, leng):
+    @length.setter
+    def length(self, leng):
         if (isinstance(leng, float) == False):
             raise Exception("Length needs to be a float value")
         if (leng <= 0.0 or leng >= 20.0):
             raise Exception("Length needs to be greater than 0.0 but lesser than 20.0")
-        self.length = leng
+        self.__length = leng
 
     @property
-    def mywidth(self):
-        return self.width
+    def width(self):
+        return self.__width
 
-    @mywidth.setter
-    def mywidth(self, wid):
+    @width.setter
+    def width(self, wid):
         if (isinstance(wid, float) == False):
             raise Exception("Width needs to be a float value")
         if (wid <= 0.0 or wid >= 20.0):
             raise Exception("Width needs to be greater than 0.0 but lesser than 20.0")
-        self.width = wid
+        self.__width = wid
 
     def calc_area(self):
         return self.length * self.width
@@ -33,6 +33,17 @@ class Rectangle:
     def calc_perim(self):
         return 2 * (self.length + self.width)
 
+if __name__ == '__main__':
+    leng = float(input("Enter the length: "))
+    wid = float(input("Enter the width: "))
+    #Перевіримо випадок коли ширину не вказано - тоді вона за умовчуванням набуває значення 1.0
+    abcd = Rectangle(leng)
+    print("The perimeter of your rectangle is", abcd.calc_perim())
+    print("The area of your rectangle is", abcd.calc_area())
+    #Тепер вкажемо ширину і обчислимо периметр і площу
+    defg = Rectangle(leng, wid)
+    print("The perimeter of your rectangle is", defg.calc_perim())
+    print("The area of your rectangle is", defg.calc_area())
 if __name__ == '__main__':
     leng = float(input("Enter the length: "))
     wid = float(input("Enter the width: "))
